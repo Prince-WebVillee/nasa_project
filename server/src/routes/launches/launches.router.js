c02289d0b4a3a8a3c23d4e64280061ca53263eaa
Prince Dwivedi <Prince>
2022-08-25 16:07:49 +0530

    post launches api added

---
+++ server/src/routes/launches/launches.router.js
@@ -0,0 +1,12 @@
+const {
+  httpGetAllLaunches,
+  httpAddNewLaunch,
+} = require("./launches.controller");
+const express = require("express");
+
+const launchesRouter = express.Router();
+
+launchesRouter.get("/", httpGetAllLaunches);
+launchesRouter.post("/", httpAddNewLaunch);
+
+module.exports = launchesRouter;
